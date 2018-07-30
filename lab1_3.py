a71974460d44e2aba6d212a1bd6f8b0d94d95b84
Abhishek Pundir <abpndr1@gmail.com>
2018-07-30 22:47:40 +0530

    Delete lab1_3.py

--- lab1_3.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-#creating placeholder of string passing value 'abhishek' to it
-
-a = tf.placeholder(tf.string)
-b = 'Hello ' + a
-with tf.Session() as sess:
-	result = sess.run(b,feed_dict={a:'abhishek'})
-	print(result)
